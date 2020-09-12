87a63535afb4b0fa5ca167787627f7374f4b72e8
tankala <ashok_tankala@yahoo.com>
2020-09-12 16:52:51 +0530

    lesson 4 & 5

---
+++ lesson-5/variables_understanding.py
@@ -0,0 +1,7 @@
+guest_name = "Somaraju"
+guest_title = "Mr."
+guest_age = "40"
+
+print(guest_title + " " + guest_name + " Gaaru, Welcome to our Village")
+print("Welcome to the legend who acheived so much in the age " + guest_age)
+print("Thank you for Visiting our Village " +  guest_title + " " + guest_name +" Gaaru")
