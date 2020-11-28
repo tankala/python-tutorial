97a20bb7d853de65d702c33fe923f805b52bc0dc
tankala <ashok_tankala@yahoo.com>
2020-11-28 17:07:39 +0530

    tuple and function examples

---
+++ python-for-beginners/lesson-17/tuple_examples.py
@@ -0,0 +1,7 @@
+traffic_rules = ("Walk on left side", "Stop your vehicle on red light", "Use high beam in high way only")
+
+# Get 1st item (item at 0th index)
+print(traffic_rules[0])
+
+# Finding the postion/index of item in tuple
+print(traffic_rules.index("Stop your vehicle on red light"))
---
+++ python-for-beginners/lesson-18/restaurant_welcome_message.py
@@ -0,0 +1,16 @@
+def welcome_message():
+    print("Hi There")
+    print("Welcome to Our Restaurant")
+    print("We serve both Asian and Western Cuisine")
+
+welcome_message()
+husband_name = input("Please give your name Sir: ")
+print("Thank you for choosing us Mr. " + husband_name)
+
+welcome_message()
+wife_name = input("Please give your name Madam: ")
+print("Thank you for choosing us Mrs. " + wife_name)
+
+welcome_message()
+kid_name = input("Please give your cute name: ")
+print("Thank you " + kid_name)
