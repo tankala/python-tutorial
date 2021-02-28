3aa7b3b513a86850934465523f97cdeeecd38268
tankala <ashok_tankala@yahoo.com>
2021-02-28 16:49:09 +0530

    exception bug fix

--- python-for-beginners/lesson-28/exception_example.py
+++ python-for-beginners/lesson-28/exception_example.py
@@ -1,5 +1,5 @@
 try:
     number = int(input("Please give me a number: "))
-    print("You have entered " + number)
+    print("You have entered " + str(number))
 except ValueError:
     print("Invalid input")
